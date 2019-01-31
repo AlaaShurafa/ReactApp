6513a75012bc86950c27417f076ac59cc9f929eb
AlaaShurafa <ialaashurafa@gmail.com>
2019-01-31 09:39:23 +0000

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,73 +1,131 @@
 class ToDo extends React.Component{
-    constructor(props){
-        super(props)
-        this.toDoListItem =['hi'];
-        this.handleInputValue = this.handleInputValue.bind(this);
-        this.handleClickItem = this.handleClickItem.bind(this);
-        this.filter = this.filter.bind(this);
-        this.state ={
-            toDo : [{
-                text : 'Hi there',
-                completedItem : false
-            }] ,
-            showFilter : false
+ 
+    state = {
+        input : '',
+        toDo : [{
+            text : 'Hi there',
+            completedItem : false
+        }] ,
+        type : {
+            all : true,
+            completed :false,
+            active : false
         }
-        
+        // showFilter : false
     }
-    handleInputValue(e){
+    handleChange = (e) =>{
+        this.setState({input : e.target.value});
+    }
+    handleInputValue = (e) => {
         e.preventDefault();
-        let inputValue = e.target.inputValue.value;
-        let newItem = this.state.toDo.concat({ text: e.target.inputValue.value, completedItem: false })
-        if (inputValue){
+        let newItem = { text:this.state.input, completedItem: false }
+        if (this.state.input){
             this.setState({
-                toDo: newItem
+                toDo: [...this.state.toDo,newItem] , input: ''
                 })
-            e.target.inputValue.value = ""
         }
-
     }
-    handleClickItem(e){
+    handleClickItem = (key) =>{
         const arrayOfItem = this.state.toDo;
-
-        arrayOfItem.map((item) => {
-            if (item.text == e.target.outerText){
-                item.completedItem = true;
-                console.log(item);
-                this.setState({
-                    toDo: arrayOfItem
-                })
+        arrayOfItem.map((item,id) => {
+            if (id == key){
+                item.completedItem = !item.completedItem;
             }
+            this.setState({
+                toDo: arrayOfItem
+            })
     })}
 
-    filter(e){
-        this.state.showFilter ? this.setState({ showFilter: false }) : this.setState({ showFilter: true });
+    filter = (type) =>{
+        // this.state.showFilter ? this.setState({ showFilter: false }) : this.setState({ showFilter: true });    
+        if(type == 'all'){
+            this.setState({
+                type: {
+                    all: true,
+                    completed: false,
+                    active: false
+                }
+            })
+        }
+        else if (type == 'completed') {
+            this.setState({
+                type: {
+                    all: false,
+                    completed: true,
+                    active: false
+                }
+            })
+        }
+        else if (type == 'active') {
+            this.setState({
+                type: {
+                    all: false,
+                    completed: false,
+                    active: true
+                }
+            })
+        }
+
     }
+    finalList = () =>{
+        
+        if(this.state.type.all){
+            console.log('hi')
+            const allList = this.state.toDo.map((item, key) =>{
+
+                return <li style={item.completedItem ? { color: '#ccc' } : { color: '#000' }}
+                    key={key} onClick={() => this.handleClickItem(key)}>
+                    {item.text}
+                </li>}
+                );
+                return allList
+            }
+        
+        if(this.state.type.completed){
+            
+            const completedList = this.state.toDo.map((item, key) => {
+                return item.completedItem ?
+                    <li style={item.completedItem ? { color: '#ccc' } : { color: '#000' }}
+                        key={key} onClick={() => this.handleClickItem(key)}>
+                        {item.text}
+                    </li>
+                    : ''
+            })               
+            return completedList
+            }
+        
+        if(this.state.type.active){
+            
+            const activeList = this.state.toDo.map((item, key) => {
+                return !item.completedItem ?
+                    <li style={item.completedItem ? { color: '#ccc' } : { color: '#000' }}
+                        key={key} onClick={() => this.handleClickItem(key)}>
+                        {item.text}
+                    </li>
+                    : ''
+            })
+            return activeList
+            }
+        }
+    
     render(){
+        
+
         return(
             <div>
                 <form onSubmit={this.handleInputValue}>
-                    <input type ="text" name="inputValue"/>
+                    <input type ="text" name="inputValue"
+                           value = {this.state.input}
+                           onChange ={this.handleChange} />
                     <button>CLick To Add</button>
                 </form>
 
                 <ol>
-                    {this.state.showFilter ?
-                        this.state.toDo.map((item, key) =>{
-                            return item.completedItem ?
-                                <li style={{ color: '#ccc'}}
-                                    key={key}>{item.text}</li>
-                                    : ''
-                            
-                        })
-
-                                
-                    : this.state.toDo.map((item, key) =>
-                            <li style={item.completedItem ? { color: '#ccc' } : { color: '#000' }}
-                                key={key} onClick={this.handleClickItem}>{item.text}</li>)
-                    }
-
+                    {this.finalList()}
                 </ol>
-                <button onClick={this.filter}>{this.state.showFilter ? 'UnFilter' : 'Filter'}</button>
+                <button onClick={() => this.filter('all')}>All</button>
+                <button onClick={() => this.filter('completed')}>Completed</button>
+                <button onClick={() => this.filter('active')}>Active</button>
             </div>
         )
     }
